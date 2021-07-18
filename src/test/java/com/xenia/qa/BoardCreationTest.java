7a276786537224170e4b9ef3ab82b85f82e3c4f3
Xenia444 <86400654+Xenia444@users.noreply.github.com>
2021-07-18 20:29:55 +0300

    new classes

--- src/test/java/com/xenia/qa/BoardCreationTest.java
+++ src/test/java/com/xenia/qa/BoardCreationTest.java
@@ -1,6 +1,7 @@
 package com.xenia.qa;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
